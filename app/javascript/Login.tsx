05c5393998c2e9486263c8d6a5d174f9ff817a3b
Sorah Fukumori <her@sorah.jp>
2021-08-29 19:42:23 +0900

    prettier

--- app/javascript/Login.tsx
+++ app/javascript/Login.tsx
@@ -1,22 +1,30 @@
 import React from "react";
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
-import { Box, Container, Button, Link, FormControl, FormLabel, FormHelperText, Input } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Button,
+  Link,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Input,
+} from "@chakra-ui/react";
 
 import Api from "./Api";
 import { ErrorAlert } from "./ErrorAlert";
 
-export interface Props {
-}
+export interface Props {}
 
 export const Login: React.FC<Props> = () => {
   const history = useHistory();
-  const [ errorAlert, setErrorAlert ] = React.useState<JSX.Element | null>(null);
-  const [ isRequesting, setIsRequesting ] = React.useState<boolean>(false);
+  const [errorAlert, setErrorAlert] = React.useState<JSX.Element | null>(null);
+  const [isRequesting, setIsRequesting] = React.useState<boolean>(false);
   const { register, handleSubmit } = useForm<{
-    email: string,
-    reference: string,
-  }>({defaultValues: {email: "", reference: ""}});
+    email: string;
+    reference: string;
+  }>({ defaultValues: { email: "", reference: "" } });
 
   const onSubmit = handleSubmit(async (data) => {
     if (isRequesting) return;
@@ -31,31 +39,54 @@ export const Login: React.FC<Props> = () => {
       } else {
         history.push("/attendee");
       }
-
     } catch (e) {
-      setErrorAlert(<Box my={2}><ErrorAlert error={e} /></Box>)
+      setErrorAlert(
+        <Box my={2}>
+          <ErrorAlert error={e} />
+        </Box>
+      );
     }
     setIsRequesting(false);
   });
 
   // TODO: link to registration page and support email
-  return <>
-    {errorAlert}
-    <Container mt="20px">
-      <p>Enter your ticket details to access.</p>
-      <form onSubmit={onSubmit}>
-        <FormControl mt={4} id="login_email" isRequired>
-          <FormLabel>Email Address</FormLabel>
-          <FormHelperText>Enter an email address registered to your ticket</FormHelperText>
-          <Input {...register("email")} type="email" autoFocus />
-        </FormControl>
-        <FormControl mt={4} id="login_reference" isRequired>
-          <FormLabel>Ticket ID (Reference Code)</FormLabel>
-            <FormHelperText>Code should be shown at <Link href="https://img.sorah.jp/x/20210827_112054_xnimUYcsKE.png" isExternal target="_blank">the upper right of a confirmation email</Link> you received.</FormHelperText>
-          <Input {...register("reference")} type="password" placeholder="ABCD-1..." />
-        </FormControl>
-        <Button mt={4} size="lg" type="submit" isLoading={isRequesting} >Log in</Button>
-      </form>
-    </Container>
-  </>;
+  return (
+    <>
+      {errorAlert}
+      <Container mt="20px">
+        <p>Enter your ticket details to access.</p>
+        <form onSubmit={onSubmit}>
+          <FormControl mt={4} id="login_email" isRequired>
+            <FormLabel>Email Address</FormLabel>
+            <FormHelperText>
+              Enter an email address registered to your ticket
+            </FormHelperText>
+            <Input {...register("email")} type="email" autoFocus />
+          </FormControl>
+          <FormControl mt={4} id="login_reference" isRequired>
+            <FormLabel>Ticket ID (Reference Code)</FormLabel>
+            <FormHelperText>
+              Code should be shown at{" "}
+              <Link
+                href="https://img.sorah.jp/x/20210827_112054_xnimUYcsKE.png"
+                isExternal
+                target="_blank"
+              >
+                the upper right of a confirmation email
+              </Link>{" "}
+              you received.
+            </FormHelperText>
+            <Input
+              {...register("reference")}
+              type="password"
+              placeholder="ABCD-1..."
+            />
+          </FormControl>
+          <Button mt={4} size="lg" type="submit" isLoading={isRequesting}>
+            Log in
+          </Button>
+        </form>
+      </Container>
+    </>
+  );
 };

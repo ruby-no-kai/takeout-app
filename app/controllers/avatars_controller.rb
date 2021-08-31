9b0a2283cc3668cdc794ee329ddc275c39ef4e17
Sorah Fukumori <her@sorah.jp>
2021-09-01 01:25:09 +0900

    avatars#show: return 404 when url is nil

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -2,6 +2,9 @@ class AvatarsController < ApplicationController
   # development purpose; override /avatars/ prefix with S3 using CDN in production
   def show
     chime_user = ChimeUser.eager_load(:attendee).find_by(handle: params[:handle])
-    redirect_to AvatarProvider.url(chime_user&.handle, chime_user&.attendee&.original_avatar_url, version: chime_user&.version)
+    url = AvatarProvider.url(chime_user&.handle, chime_user&.attendee&.original_avatar_url, version: chime_user&.version)
+    return redirect_to url if url
+
+    render(status: 404, json: {ok: false, error: 'not found'}.to_json)
   end
 end

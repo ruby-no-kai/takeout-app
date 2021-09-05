23cac2365959d645cc7721b24e284ba3c99e10ad
Sorah Fukumori <her@sorah.jp>
2021-09-05 14:26:00 +0900

    TrackView: remove unnecessary <></>

--- app/javascript/TrackView.tsx
+++ app/javascript/TrackView.tsx
@@ -24,50 +24,48 @@ export const TrackView: React.FC<Props> = ({ track, streamOptionsState }) => {
   // TODO: Chakra 側のブレークポイントの調整
   // TODO: hide chat button
   return (
-    <>
-      <Container maxW={["auto", "auto", "auto", "1700px"]}>
-        <Flex alignItems="top" justifyContent="space-between" direction={["column", "column", "column", "row"]}>
-          <Box w="100%">
-            <React.Suspense
-              fallback={
-                <AspectRatio ratio={16 / 9}>
-                  <Skeleton w="100%" h="100%" />
-                </AspectRatio>
-              }
-            >
-              <TrackVideo track={track} streamOptions={streamOptionsState[0]} />
+    <Container maxW={["auto", "auto", "auto", "1700px"]}>
+      <Flex alignItems="top" justifyContent="space-between" direction={["column", "column", "column", "row"]}>
+        <Box w="100%">
+          <React.Suspense
+            fallback={
+              <AspectRatio ratio={16 / 9}>
+                <Skeleton w="100%" h="100%" />
+              </AspectRatio>
+            }
+          >
+            <TrackVideo track={track} streamOptions={streamOptionsState[0]} />
+          </React.Suspense>
+          {streamOptions.caption ? (
+            <React.Suspense fallback={<Skeleton w="100%" h="80px" />}>
+              <TrackCaption
+                track={track}
+                onUnsubscribe={() => {
+                  setStreamOptions({ ...streamOptions, caption: false });
+                }}
+              />
             </React.Suspense>
-            {streamOptions.caption ? (
-              <React.Suspense fallback={<Skeleton w="100%" h="80px" />}>
-                <TrackCaption
-                  track={track}
-                  onUnsubscribe={() => {
-                    setStreamOptions({ ...streamOptions, caption: false });
-                  }}
-                />
-              </React.Suspense>
-            ) : null}
-          </Box>
-
-          {streamOptions.chat && track.chat ? (
-            <Box maxW={["auto", "auto", "auto", "400px"]} minH="400px" w="100%" ml={3}>
-              <React.Suspense fallback={<Skeleton w="100%" h="100%" />}>
-                <TrackChat track={track} />
-              </React.Suspense>
-            </Box>
           ) : null}
-        </Flex>
+        </Box>
 
-        <Flex alignItems="top" justifyContent="space-between" direction={["column", "column", "column", "row"]}>
-          <Box w="100%">
-            <React.Suspense fallback={<Skeleton w="100%" h="20px" />}>
-              <TrackCardView card={track.card} nav={trackOptionsSelector} />
+        {streamOptions.chat && track.chat ? (
+          <Box maxW={["auto", "auto", "auto", "400px"]} minH="400px" w="100%" ml={3}>
+            <React.Suspense fallback={<Skeleton w="100%" h="100%" />}>
+              <TrackChat track={track} />
             </React.Suspense>
           </Box>
-          <Box maxW={["auto", "auto", "auto", "400px"]} w="100%" ml={3} />
-        </Flex>
-      </Container>
-    </>
+        ) : null}
+      </Flex>
+
+      <Flex alignItems="top" justifyContent="space-between" direction={["column", "column", "column", "row"]}>
+        <Box w="100%">
+          <React.Suspense fallback={<Skeleton w="100%" h="20px" />}>
+            <TrackCardView card={track.card} nav={trackOptionsSelector} />
+          </React.Suspense>
+        </Box>
+        <Box maxW={["auto", "auto", "auto", "400px"]} w="100%" ml={3} />
+      </Flex>
+    </Container>
   );
 };
 

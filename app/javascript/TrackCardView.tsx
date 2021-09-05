bd31402a879e3f306e45fa1c3596466538a25feb
Sorah Fukumori <her@sorah.jp>
2021-09-05 19:35:47 +0900

    avatar and speaker names in topic

--- app/javascript/TrackCardView.tsx
+++ app/javascript/TrackCardView.tsx
@@ -6,7 +6,6 @@ import { Flex, Box } from "@chakra-ui/react";
 import { TrackCard } from "./Api";
 
 const TrackTopic = loadable(() => import("./TrackTopic"));
-const TrackSpeaker = loadable(() => import("./TrackSpeaker"));
 
 export interface Props {
   card: TrackCard | null;
@@ -17,9 +16,6 @@ export const TrackCardView: React.FC<Props> = ({ card, nav }) => {
   return (
     <Box>
       <TrackTopic card={card} topicNav={nav} />
-      {card?.speakers?.map((s) => (
-        <TrackSpeaker key={`${s.name}-${s.avatar_url}`} speaker={s} />
-      ))}
     </Box>
   );
 };
--- app/javascript/TrackTopic.tsx
+++ app/javascript/TrackTopic.tsx
@@ -1,9 +1,13 @@
 import React from "react";
 
-import { Flex, Box, Heading, Text } from "@chakra-ui/react";
+import { Flex, Box, Heading, Text, Link } from "@chakra-ui/react";
 import { Tag, HStack } from "@chakra-ui/react";
 
-import type { TrackCard } from "./Api";
+import type { TrackCard, Speaker } from "./Api";
+
+import { Colors } from "./theme";
+import { GitHubIcon } from "./GitHubIcon";
+import { TwitterIcon } from "./TwitterIcon";
 
 import { SpeakerAvatar } from "./SpeakerAvatar";
 
@@ -24,15 +28,15 @@ export const TrackTopic: React.FC<Props> = ({ card, topicNav }) => {
 
   return (
     <Box>
-      <Flex justify="space-between" align="center" w="100%">
-        <HStack flexGrow={1} flexShrink={0} flexBasis={0}>
+      <Flex justify="space-between" align="center" w="100%" alignItems="top" mb="18px">
+        <HStack flexGrow={1} flexShrink={0} flexBasis={0} alignItems="top">
           {card.speakers && card.speakers.length > 0 ? (
-            <Box w="100%" maxW="64px">
+            <Box w="100%" maxW="64px" mr="12px">
               <SpeakerAvatar speakers={card.speakers} />
             </Box>
           ) : null}
-          <Text as="div" lineHeight="29px">
-            <Heading as="h2" fontSize="30px" lineHeight="36px">
+          <Box as="div">
+            <Heading as="h2" fontSize="30px" lineHeight="36px" fontWeight="500" mt="-4px">
               {topic.title}
             </Heading>
             <Box mt="2px">
@@ -42,12 +46,48 @@ export const TrackTopic: React.FC<Props> = ({ card, topicNav }) => {
                 </Tag>
               ))}
             </Box>
-          </Text>
+            {card.speakers && card.speakers.length > 0 ? (
+              <Box>
+                {card.speakers.map((s) => (
+                  <TrackTopicSpeaker key={s.avatar_url} speaker={s} />
+                ))}
+              </Box>
+            ) : null}
+          </Box>
         </HStack>
         <Box>{topicNav}</Box>
       </Flex>
-      <Text my="11px">{topic.description}</Text>
+      <Box py="18px" borderTop="1px solid" borderColor={Colors.border}>
+        <Text as="p">{topic.description}</Text>
+      </Box>
     </Box>
+  );
+};
+
+const TrackTopicSpeaker: React.FC<{ speaker: Speaker }> = ({ speaker }) => {
+  const primaryAlias = speaker.github_id || speaker.twitter_id;
+  const primaryLink =
+    (speaker.github_id && `https://github.com/${speaker.github_id}`) ||
+    (speaker.twitter_id && `https://twitter.com/${speaker.twitter_id}`);
+  return (
+    <HStack as="p" fontSize="16px" fontWeight="400" h="20px" lineHeight="24px" mt="3px" color={Colors.secondary}>
+      <Text as="span">{speaker.name}</Text>
+      {primaryAlias && primaryLink ? (
+        <Link isExternal href={primaryLink} m={0} ml={1}>
+          @{primaryAlias}
+        </Link>
+      ) : null}
+      {speaker.github_id ? (
+        <Link isExternal href={`https://github.com/${speaker.github_id}`} m={0} ml={1}>
+          <GitHubIcon boxSize="16px" />
+        </Link>
+      ) : null}
+      {speaker.twitter_id ? (
+        <Link isExternal href={`https://twitter.com/${speaker.twitter_id}`} m={0} ml={1}>
+          <TwitterIcon boxSize="16px" />
+        </Link>
+      ) : null}
+    </HStack>
   );
 };
 

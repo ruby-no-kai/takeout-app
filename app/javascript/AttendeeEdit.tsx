ff7f7dfde7e71eb52bf76332182a94ad6a04507e
Sorah Fukumori <her@sorah.jp>
2021-09-06 23:59:19 +0900

    AttendeeEdit: design

--- app/javascript/AttendeeEdit.tsx
+++ app/javascript/AttendeeEdit.tsx
@@ -1,15 +1,28 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useHistory } from "react-router-dom";
-import { Box, Container, Button, Link, FormControl, FormLabel, FormHelperText, Input } from "@chakra-ui/react";
-import { Center, Circle, Image } from "@chakra-ui/react";
+import {
+  VStack,
+  HStack,
+  Box,
+  Container,
+  Button,
+  Link,
+  Text,
+  Heading,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Input,
+  Avatar,
+  Spinner,
+} from "@chakra-ui/react";
 
 import Api from "./Api";
+import { Colors } from "./theme";
 import { ErrorAlert } from "./ErrorAlert";
 
-export interface Props {}
-
-export const AttendeeEdit: React.FC<Props> = () => {
+export const AttendeeEdit: React.FC = () => {
   const { data: conferenceData } = Api.useConference();
   const { data: session, error: sessionError } = Api.useSession();
   const history = useHistory();
@@ -60,53 +73,75 @@ export const AttendeeEdit: React.FC<Props> = () => {
 
   if (!session?.attendee) {
     return (
-      <>
-        {sessionError ? (
-          <Box my={2}>
-            <ErrorAlert error={sessionError} />
-          </Box>
-        ) : null}
-        <p>Loading</p>
-      </>
+      <Container maxW={["auto", "auto", "auto", "1000px"]} px="15px" py="22px">
+        <VStack>
+          {sessionError ? (
+            <Box my={2}>
+              <ErrorAlert error={sessionError} />
+            </Box>
+          ) : null}
+          <Spinner size="xl" />
+        </VStack>
+      </Container>
     );
   }
 
-  // TODO: ポリシーページへのリンク
-  // TODO: !ready のときは Save and continue ボタンにしたい
-
   return (
-    <>
-      {errorAlert}
-      <Container mt="20px">
-        <p>Confirm your name and avatar used at live chat (these informations may be shared with other attendees):</p>
-        <Center my={4}>
-          <Circle w="200px" h="200px">
-            <Image src={session.attendee.avatar_url} alt="" />
-          </Circle>
-        </Center>
-        <form onSubmit={onSubmit}>
-          <FormControl mt={4} id="login_reference" isRequired>
-            <FormLabel>Name</FormLabel>
-            <Input {...register("name")} autoFocus />
-          </FormControl>
-
-          <FormControl mt={4} id="login_email">
-            <FormLabel>Gravatar Email</FormLabel>
-            <FormHelperText>
-              We use avatar images registered on{" "}
-              <Link href="https://www.gravatar.com" isExternal>
-                Gravatar
+    <Container maxW={["auto", "auto", "auto", "1000px"]} px="15px" py="22px">
+      <VStack justify="start" alignItems="start" spacing="30px">
+        <Heading as="h2" color={Colors.main}>
+          Settings
+        </Heading>
+        <HStack spacing="30px">
+          <Avatar size="xl" bg={Colors.defaultAvatarBg} src={session.attendee.avatar_url} loading="lazy" />
+          <Box maxW="750px">
+            <Text mb={2}>
+              Confirm your name and avatar used at live chat. These informations may be shared with other attendees once
+              submitted.
+            </Text>
+            <Text>
+              Be remember to abide by{" "}
+              <Link href="https://rubykaigi.org/2021-takeout/policies" isExternal textDecoration="underline">
+                our policies
               </Link>
-              . If you desire to choose alternate email address for your Gravatar image, then fill the following field.
-            </FormHelperText>
-            <Input {...register("gravatar_email")} type="email" placeholder="(leave empty to remain unchanged)" />
-          </FormControl>
-          <Button mt={4} size="lg" type="submit" isLoading={isRequesting}>
-            Save
-          </Button>
+              .
+            </Text>
+          </Box>
+        </HStack>
+        <form onSubmit={onSubmit}>
+          <VStack justify="start" alignItems="start" spacing="30px">
+            <FormControl id="login_reference" isRequired>
+              <FormLabel>Name</FormLabel>
+              <FormHelperText>Feel free to use nicknames, usernames, or handles :)</FormHelperText>
+              <Input {...register("name")} maxW="460px" autoFocus />
+            </FormControl>
+
+            <FormControl id="login_email">
+              <FormLabel>Gravatar Email Address</FormLabel>
+              <FormHelperText>
+                We use avatar images registered on{" "}
+                <Link href="https://www.gravatar.com" isExternal textDecoration="underline">
+                  Gravatar
+                </Link>
+                . Fill the following field if you desire to choose different email address for your Gravatar image.
+              </FormHelperText>
+              <Input
+                {...register("gravatar_email")}
+                type="email"
+                maxW="460px"
+                placeholder="(leave empty to remain unchanged)"
+              />
+            </FormControl>
+
+            <Button type="submit" w="160px" h="46px" colorScheme="rk" isLoading={isRequesting}>
+              {session.attendee.is_ready ? "Save" : "Continue"}
+            </Button>
+          </VStack>
         </form>
-      </Container>
-    </>
+
+        {errorAlert}
+      </VStack>
+    </Container>
   );
 };
 export default AttendeeEdit;

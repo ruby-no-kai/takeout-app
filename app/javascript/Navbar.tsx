3cb3c9f92315541c24f4a97804b6048b605eadff
Sorah Fukumori <her@sorah.jp>
2021-09-06 22:40:30 +0900

    Navbar: use Logo svg

--- app/javascript/Navbar.tsx
+++ app/javascript/Navbar.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Colors } from "./theme";
-import { HStack, Flex, Heading, Text, Link } from "@chakra-ui/react";
+import { Box, HStack, Flex, Heading, Text, Link } from "@chakra-ui/react";
+import { VisuallyHidden } from "@chakra-ui/react";
 import { Link as RouterLink } from "react-router-dom";
 
 import { EventNoteIcon } from "./EventNoteIcon";
 import { AccountCircleIcon } from "./AccountCircleIcon";
+import { Logo } from "./Logo";
 
 import Api from "./Api";
 
@@ -16,14 +18,16 @@ export const Navbar: React.FC = () => {
   return (
     <>
       <Flex as="nav" justify="space-between" alignItems="center" w="100%" h="56px" px="18px" bgColor={Colors.bg}>
-        <Heading as="h1" size="lg" fontSize="22px">
+        <Heading as="h1" size="lg" fontSize="22px" css={{ "& svg": { height: "22px", width: "auto" } }}>
           {conferenceData?.conference ? (
             <Link as={RouterLink} to={`/tracks/${conferenceData.conference.default_track}`} color={Colors.main}>
-              RubyKaigi Takeout 2021
+              <VisuallyHidden>RubyKaigi Takeout 2021</VisuallyHidden>
+              <Logo />
             </Link>
           ) : (
             <Text as="span" color={Colors.main}>
-              RubyKaigi Takeout 2021
+              <VisuallyHidden>RubyKaigi Takeout 2021</VisuallyHidden>
+              <Logo />
             </Text>
           )}
         </Heading>

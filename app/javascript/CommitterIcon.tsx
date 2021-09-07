716fa59796e9acd9f3650e16cbbe927a424296e1
Sorah Fukumori <her@sorah.jp>
2021-09-08 07:01:17 +0900

    CommitterIcon: tweak

--- app/javascript/CommitterIcon.tsx
+++ app/javascript/CommitterIcon.tsx
@@ -3,6 +3,6 @@ import { createIcon } from "@chakra-ui/icons";
 
 export const CommitterIcon = createIcon({
   displayName: "Committer",
-  viewBox: "0 0 18 18",
-  path: <path d="M3.6 0L0 5.4L9 18L18 5.4L14.4 0H3.6Z" fill="currentColor" />,
+  viewBox: "0 0 24 24",
+  path: <path d="M6.6 4L3 9.4L12 20L21 9.4L17.4 4H6.6Z" fill="currentColor" />,
 });

3f57296d2de759613a6c37f8dc5606b0989ca6f9
Sorah Fukumori <her@sorah.jp>
2022-08-27 14:29:19 +0900

    Rails 7 & Ruby 3.1

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -6,18 +6,18 @@ class AvatarsController < ApplicationController
     when handle.start_with?('u_')
       chime_user = ChimeUser.eager_load(:attendee).find_by(handle: handle)
       url = AvatarProvider.url(chime_user&.handle, chime_user&.attendee&.original_avatar_url, version: chime_user&.version)
-      return redirect_to url if url
+        return redirect_to(url, allow_other_host: true) if url
 
     when handle.start_with?('s_')
       speaker = ConferenceSpeaker.find_by(slug: handle[2..-1])
       if speaker
-        return redirect_to speaker.original_avatar_url
+        return redirect_to(speaker.original_avatar_url, allow_other_host: true)
       end
 
     when handle.match(/^ps?_(.+)$/) # ps_ = small sponsor logo, p_ = large sponsor logo
       sponsor = ConferenceSponsorship.find_by(sponsor_app_id: $1)
       if sponsor
-        return redirect_to sponsor.avatar_url
+        return redirect_to(sponsor.avatar_url, allow_other_host: true)
       end
 
     end

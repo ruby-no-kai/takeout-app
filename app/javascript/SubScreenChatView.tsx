fb357d787b2fdee32f46f1e56307375a42ed3f47
Sorah Fukumori <her@sorah.jp>
2022-09-08 15:55:42 +0900

    SubScreenChatView: increase size

--- app/javascript/SubScreenChatView.tsx
+++ app/javascript/SubScreenChatView.tsx
@@ -11,7 +11,7 @@ export const SubScreenChatView: React.FC<{ track: Track }> = ({ track }) => {
   return (
     <React.Suspense fallback={<Skeleton w="100%" h="100%" />}>
       {/* Adjust to w="47.5vw" h="51vw"  w=912 h=979 */}
-      <Box w={`${47.5 * 0.6}vw`} h={`${51 * 0.6}vw`} css={{ transform: `scale(${1 / 0.6})`, transformOrigin: "0 0" }}>
+      <Box w={`${47.5 * 0.4}vw`} h={`${51 * 0.4}vw`} css={{ transform: `scale(${1 / 0.4})`, transformOrigin: "0 0" }}>
         <TrackChat track={track} hideForm={true} disableAdminControl={true} disableSponsorPromo={true} />
       </Box>
     </React.Suspense>

ad980a35dba7097ec2463c3f705cd62d31217dcb
Sorah Fukumori <her@sorah.jp>
2022-09-09 13:19:06 +0900

    EmitChatMessagePinJob: use system channel for subscreen

--- app/jobs/emit_chat_message_pin_job.rb
+++ app/jobs/emit_chat_message_pin_job.rb
@@ -4,16 +4,24 @@ class EmitChatMessagePinJob < ApplicationJob
 
     @chimemessaging = Aws::ChimeSDKMessaging::Client.new(region: 'us-east-1', logger: Rails.logger)
     pins.each do |_|
-      channel_arn = Conference.data.fetch(:tracks).dig(_.track, :chime, :channel_arn)
-      next unless channel_arn
-
       @chimemessaging.send_channel_message(
         chime_bearer: Conference.data.fetch(:chime).fetch(:app_user_arn),
-        channel_arn: channel_arn,
+        channel_arn: Conference.data.fetch(:chime).fetch(:systems_channel_arn),
         content: {control: {pin: _.as_json}}.to_json,
         type: 'STANDARD',
         persistence: 'NON_PERSISTENT',
       )
+
+      channel_arn = Conference.data.fetch(:tracks).dig(_.track, :chime, :channel_arn)
+      if channel_arn
+        @chimemessaging.send_channel_message(
+          chime_bearer: Conference.data.fetch(:chime).fetch(:app_user_arn),
+          channel_arn: channel_arn,
+          content: {control: {pin: _.as_json}}.to_json,
+          type: 'STANDARD',
+          persistence: 'NON_PERSISTENT',
+        )
+      end
     end
   end
 end

5ffbc3abedf4f60a4c6dce66fc36fc96288b9343
Sorah Fukumori <her@sorah.jp>
2022-09-10 14:04:13 +0900

    screen signage mode

--- app/javascript/IntermissionScreen.tsx
+++ app/javascript/IntermissionScreen.tsx
@@ -1,4 +1,6 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useState, useEffect } from "react";
+
+import dayjs from "dayjs";
 
 import { HStack, VStack, Heading, Flex, Box, Container, Image, Text } from "@chakra-ui/react";
 import { AspectRatio } from "@chakra-ui/react";
@@ -15,6 +17,7 @@ import { ChatProvider } from "./ChatProvider";
 import { useChat } from "./ChatProvider";
 import { ChatUpdate } from "./ChatSession";
 import { KioskLogin } from "./KioskLogin";
+import { useSearchParams } from "react-router-dom";
 
 export const IntermissionScreen: React.FC = () => {
   return (
@@ -32,6 +35,8 @@ export const IntermissionScreen: React.FC = () => {
 };
 
 export const IntermissionScreenInner: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const isSignage = (searchParams.get("signage") ?? "").length > 0;
   const chat = useChat();
   const systemsChannel = chat.systems_channel_arn;
 
@@ -59,14 +64,74 @@ export const IntermissionScreenInner: React.FC = () => {
 
   return (
     <Flex h="100%" w="100%" justify="space-between" direction="row">
-      <Box h="100%">
-        <ScreenAnnounceView />
-      </Box>
+      <Box h="100%">{isSignage ? <ScreenSignageRotation /> : <ScreenAnnounceView />}</Box>
       <Box h="100%">
         <ScreenSponsorRotation />
       </Box>
     </Flex>
   );
 };
+
+const TICK_INTERVAL = 1;
+const ROTATE_INTERVAL = 12;
+const ScreenSignageRotation: React.FC = () => {
+  const [tick, setTick] = useState(dayjs().unix());
+  const { data } = Api.useVenueAnnouncements();
+  useEffect(() => {
+    setTick(dayjs().unix());
+    const timer = setInterval(() => {
+      setTick(dayjs().unix());
+    }, TICK_INTERVAL * 1000);
+    return () => clearInterval(timer);
+  }, []);
+  if (!data) return <></>;
+
+  console.log("useVenueAnnouncements", data);
+
+  const entries = data.venue_announcements.filter((v) => !v.only_subscreen);
+  console.log("useVenueAnnouncements/entries", entries);
+
+  const now = tick === -1 ? dayjs().unix() : tick;
+  const idx = Math.floor(now / ROTATE_INTERVAL) % (entries.length + 1);
+  const ann = idx == 0 ? null : entries[idx - 1];
+
+  console.log("ann", ann);
+  return (
+    <>
+      {ann ? (
+        <Flex w="45vw" h="100%" direction="column">
+          <Box css={{ "& svg": { height: "2.5vw", width: "auto" } }}>
+            <Logo />
+          </Box>
+          <Box w="100%" flexGrow={1} flexShrink={0} flexBasis={0}>
+            <Flex
+              w="100%"
+              h="100%"
+              direction="column"
+              justify="space-around"
+              color={Colors.textDefault}
+              textAlign="center"
+            >
+              <Text fontWeight="500" fontSize="3vw" lineHeight="5vw">
+                {returnToBr(ann.content)}
+              </Text>
+            </Flex>
+          </Box>
+        </Flex>
+      ) : (
+        <ScreenAnnounceView />
+      )}
+    </>
+  );
+};
+
+// XXX: returnToBr dupe
+function returnToBr(text: string) {
+  const elems = text
+    .split("\n")
+    .flatMap((v, i) => [<React.Fragment key={`${i}t`}>{v}</React.Fragment>, <br key={`${i}b`} />]);
+  elems.pop();
+  return elems;
+}
 
 export default IntermissionScreen;

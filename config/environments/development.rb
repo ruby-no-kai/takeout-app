84ba5a1d6c63ada94f018db421a8a5128c0daadc
Sorah Fukumori <her@sorah.jp>
2023-05-01 02:43:58 +0900

    development: allow trycloudflare\.com

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -79,5 +79,6 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  config.hosts << /[a-z0-9]+\.ngrok\.io/
+  config.hosts << /[a-z0-9]+\.ngrok\.io$/
+  config.hosts << /[a-z0-9\-]+\.trycloudflare\.com$/
 end
